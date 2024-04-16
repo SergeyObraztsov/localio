23b2726fef4d6952f5331554c9ca11f34173d82f
SergeyObraztsov <s.obraztsov.v@gmail.com>
2024-04-16 14:38:39 +0400

    config change

--- next.config.js
+++ next.config.js
@@ -6,8 +6,7 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  experimental: { serverActions: { bodySizeLimit: 10000 } },
-  serverActions: { bodySizeLimit: 10000 }
+  serverActions: { bodySizeLimit: '10mb' }
 };
 
 export default config;
